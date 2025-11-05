20c640e18a93fe271e6179c9b8961fbfb3d68726
krv006 <rvkamronbek@gmail.com>
2025-11-05 16:28:16 +0500

    api create

---
+++ apps/serializers/__init__.py
@@ -0,0 +1,2 @@
+from apps.serializers.product_serializer import *
+from apps.serializers.user_serializer import *
---
+++ apps/views/__init__.py
@@ -0,0 +1,2 @@
+from apps.views.product_view import *
+from apps.views.user_view import *
